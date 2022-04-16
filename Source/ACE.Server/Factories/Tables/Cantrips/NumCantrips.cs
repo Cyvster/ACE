287d4570281ac93a2462d446769e78255d2ea149
Cyvster <41338761+Cyvster@users.noreply.github.com>
2022-04-15 20:54:12 -0500

    Update NumCantrips.cs
    Increased chance of more cantrips on loot
    Increased chance of higher tier cantrips on loot

--- Source/ACE.Server/Factories/Tables/Cantrips/NumCantrips.cs
+++ Source/ACE.Server/Factories/Tables/Cantrips/NumCantrips.cs
@@ -9,56 +9,56 @@ namespace ACE.Server.Factories.Tables
     {
         private static ChanceTable<int> T1_NumCantrips = new ChanceTable<int>()
         {
-            ( 0, 0.95f ),
-            ( 1, 0.05f ),
+            ( 0, 0.05f ),
+            ( 1, 0.95f ),
         };
 
         private static ChanceTable<int> T2_NumCantrips = new ChanceTable<int>()
         {
-            ( 0, 0.90f ),
-            ( 1, 0.10f ),
+            ( 0, 0.95f ),
+            ( 1, 0.05f ),
         };
 
         private static ChanceTable<int> T3_NumCantrips = new ChanceTable<int>()
         {
-            ( 0, 0.725f ),
-            ( 1, 0.250f ),
-            ( 2, 0.025f ),
+            ( 0, 0.10f ),
+            ( 1, 0.20f ),
+            ( 2, 0.70f ),
         };
 
         private static ChanceTable<int> T4_NumCantrips = new ChanceTable<int>()
         {
-            ( 0, 0.62f ),
-            ( 1, 0.32f ),
-            ( 2, 0.055f ),
-            ( 3, 0.005f ),
+            ( 0, 0.10f ),
+            ( 1, 0.10f ),
+            ( 2, 0.10f ),
+            ( 3, 0.70f ),
         };
 
         private static ChanceTable<int> T5_NumCantrips = new ChanceTable<int>()
         {
-            ( 0, 0.40f ),
-            ( 1, 0.42f ),
-            ( 2, 0.155f ),
-            ( 3, 0.024f ),
-            ( 4, 0.001f ),
+            ( 0, 0.05f ),
+            ( 1, 0.05f ),
+            ( 2, 0.05f ),
+            ( 3, 0.10f ),
+            ( 4, 0.75f ),
         };
 
         private static ChanceTable<int> T6_NumCantrips = new ChanceTable<int>()
         {
-            ( 0, 0.25f ),
-            ( 1, 0.40f ),
-            ( 2, 0.25f ),
-            ( 3, 0.08f ),
-            ( 4, 0.019f ),
-            ( 5, 0.001f ),
+            ( 0, 0.05f ),
+            ( 1, 0.05f ),
+            ( 2, 0.05f ),
+            ( 3, 0.05f ),
+            ( 4, 0.05f ),
+            ( 5, 0.75f ),
         };
 
         private static ChanceTable<int> T7_T8_NumCantrips = new ChanceTable<int>()
         {
-            ( 1, 0.81f ),
-            ( 2, 0.17f ),
-            ( 3, 0.016f ),
-            ( 4, 0.004f ),
+            ( 1, 0.05f ),
+            ( 2, 0.05f ),
+            ( 3, 0.10f ),
+            ( 4, 0.75f ),
         };
 
         private static readonly List<ChanceTable<int>> numCantrips = new List<ChanceTable<int>>()
@@ -109,17 +109,17 @@ namespace ACE.Server.Factories.Tables
 
         private static ChanceTable<int> T7_CantripLevel = new ChanceTable<int>()
         {
-            ( 1, 0.15f ),
-            ( 2, 0.60f ),
-            ( 3, 0.25f )
+            ( 1, 0.01f ),
+            ( 2, 0.01f ),
+            ( 3, 0.98f )
         };
 
         private static ChanceTable<int> T8_CantripLevel = new ChanceTable<int>()
         {
-            ( 1, 0.02f ),
-            ( 2, 0.46f ),
-            ( 3, 0.42f ),
-            ( 4, 0.10f )
+            ( 1, 0.005f ),
+            ( 2, 0.005f ),
+            ( 3, 0.010f ),
+            ( 4, 0.980f )
         };
 
         private static readonly List<ChanceTable<int>> cantripLevels = new List<ChanceTable<int>>()
