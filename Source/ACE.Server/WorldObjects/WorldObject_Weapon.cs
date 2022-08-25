e82d874fdfe631dd5f1209e33dc3082774d450d7
Cyvster <41338761+Cyvster@users.noreply.github.com>
2022-08-25 11:40:06 -0500

    Update WorldObject_Weapon.cs
    removed melee cleave. it was addeded to creature_melee as a percentage chance instead of a 100% chance for all melee here

--- Source/ACE.Server/WorldObjects/WorldObject_Weapon.cs
+++ Source/ACE.Server/WorldObjects/WorldObject_Weapon.cs
@@ -57,7 +57,7 @@ namespace ACE.Server.WorldObjects
                 if (!IsCleaving)
                     return 0;
 
-                return Math.Min(1, (GetProperty(PropertyInt.Cleaving).GetValueOrDefault() - 1));
+                return GetProperty(PropertyInt.Cleaving).Value - 1;
             }
         }
 
