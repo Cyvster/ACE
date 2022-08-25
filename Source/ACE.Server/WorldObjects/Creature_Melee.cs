2a96a6ad3dd44a4648d042fcc76375645a230f69
Cyvster <41338761+Cyvster@users.noreply.github.com>
2022-08-25 12:26:03 -0500

    Update Creature_Melee.cs
    lowered chance to cleave

--- Source/ACE.Server/WorldObjects/Creature_Melee.cs
+++ Source/ACE.Server/WorldObjects/Creature_Melee.cs
@@ -116,8 +116,8 @@ namespace ACE.Server.WorldObjects
             if (!weapon.IsCleaving) return null;
             
             var minChance = .01;
-            var maxChance = 1.0;
-            var skillCap = 1200.0;
+            var maxChance = .5;
+            var skillCap = 900.0;
             var skill = GetCreatureSkill(weapon.WeaponSkill).Current; // or .Current or .Base if you want the buffed skill.  Might need a null check on WeaponSkill?
             var cleaveChance = Math.Clamp(minChance + (maxChance - minChance) * (skill / skillCap), minChance, maxChance);
             if (cleaveChance < ACE.Common.ThreadSafeRandom.Next(0.0f, 1.0f))
