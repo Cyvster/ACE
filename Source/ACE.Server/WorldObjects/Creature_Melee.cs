c26d4340cec6a5966e7f7178cd3c0706b8d53bf0
Cyvster <41338761+Cyvster@users.noreply.github.com>
2022-08-26 04:42:58 -0500

    Update Creature_Melee.cs
    Changed formula on cleavechance. I think it was backwards.

--- Source/ACE.Server/WorldObjects/Creature_Melee.cs
+++ Source/ACE.Server/WorldObjects/Creature_Melee.cs
@@ -120,7 +120,7 @@ namespace ACE.Server.WorldObjects
             var skillCap = 32000.0;
             var skill = GetCreatureSkill(weapon.WeaponSkill).Current; // or .Current or .Base if you want the buffed skill.  Might need a null check on WeaponSkill?
             var cleaveChance = Math.Clamp(minChance + (maxChance - minChance) * (skill / skillCap), minChance, maxChance);
-            if (cleaveChance < ACE.Common.ThreadSafeRandom.Next(0.0f, 1.0f))
+            if (cleaveChance > ACE.Common.ThreadSafeRandom.Next(0.0f, 1.0f))
                 return new List<Creature>();    // make sure it's a blank list instead of null
 
             // sort visible objects by ascending distance
